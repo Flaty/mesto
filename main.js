3a52fe24e2d162904848b7e476a0d1040a7f1227
kaneki <hitomi.jpeg@gmail.com>
2022-04-13 12:12:06 +0000

    fix: mainjs

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ const profileEditBtn = document.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup__bg');
 const profileCloseBtn = document.querySelector('.popup__close');
 const profileName = document.querySelector('.profile__info-name');
-const profileDics = document.querySelector('.profile__info-work');
+const profileProf = document.querySelector('.profile__info-work');
 const profileInputName = document.querySelector('.popup__input_type_name');
-const profileInputDisc = document.querySelector('.popup__input_type_prof');
-const submit = document.querySelector('.popup__submit-btn');
-const formElement = document.querySelectorAll('.popup__input');
-const likeActive = document.querySelector('.activeheart');
-const likeUnActive = document.querySelectorAll('.element__info-like')
+const profileInputProf = document.querySelector('.popup__input_type_prof');
+const btnPopupSubmit = document.querySelector('.popup__submit-btn');
+const btnLikeActive = document.querySelector('.activeheart');
+const btnLike = document.querySelectorAll('.element__info-like')
 
 
 function popupOpen() {
@@ -28,7 +27,7 @@ profileCloseBtn.addEventListener('click', () => {
 profileEditBtn.addEventListener('click', function(evt) {
   evt.preventDefault();
   profileInputName.value = profileName.textContent;
-  profileInputDisc.value = profileDics.textContent;
+  profileInputProf.value = profileDics.textContent;
 
   popupOpen()
 
@@ -37,7 +36,7 @@ profileEditBtn.addEventListener('click', function(evt) {
 submit.addEventListener('click', function(e) {
   e.preventDefault;
   profileName.textContent = profileInputName.value;
-  profileDics.textContent = profileInputDisc.value;
+  profileDics.textContent = profileInputProf.value;
 
   popupClose()
 })

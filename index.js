9644cd0fdb012afb40f226dcb2bcf7da88b2b07d
kaneki <hitomi.jpeg@gmail.com>
2022-04-20 17:53:32 +0300

    fix

--- index.js
+++ index.js
@@ -5,35 +5,32 @@ const profileName = document.querySelector('.profile__info-name');
 const profileProf = document.querySelector('.profile__info-work');
 const profileInputName = document.querySelector('.popup__input_type_name');
 const profileInputProf = document.querySelector('.popup__input_type_prof');
-const btnPopupSubmit = document.querySelector('.popup__submit-btn');
-const btnLikeActive = document.querySelector('.activeheart');
-const btnLike = document.querySelectorAll('.element__info-like');
-const formev = document.querySelector('.form');
+const formSubmit = document.querySelector('.form');
 
 function popupClose() {
-  popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 }
 
 function popupOpen() {
   profileInputName.value = profileName.textContent;
   profileInputProf.value = profileProf.textContent;
 
-  popup.classList.add('popup__opened');
+  popup.classList.add('popup_opened');
 }
 
-function popupSbmt(Event) {
-  Event.preventDefault();
+function popupSbmt(event) {
+  event.preventDefault();
 
   profileName.textContent = profileInputName.value;
   profileProf.textContent = profileInputProf.value;
 
-  popup.classList.remove('popup__opened')
+  popupClose();
 }
 
 profileCloseBtn.addEventListener('click', popupClose)
 
 profileEditBtn.addEventListener('click', popupOpen)
 
-formev.addEventListener('submit', popupSbmt)
+formSubmit.addEventListener('submit', popupSbmt)
 
 
